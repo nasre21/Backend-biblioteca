62ecd15a91a7108b092e2ecff6e060f9a5ac89d2
Anismelow <anyellmendozalopez@gmail.com>
2023-06-21 13:27:37 +0200

    cambio de rutas empleados

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from routes.empleados import *
 app = Flask(__name__)
 
 # Rutas empleados
-app.route('/empleado', methods=['POST'])(add_empleado)
-app.route('/', methods=['GET'])(get_empleados)
+app.route('/empleado_add', methods=['POST'])(add_empleado)
+app.route('/empleados', methods=['GET'])(get_empleados)
 app.route('/empleado/<int:id_empleado>')(obtener_empleado_por_id)
-app.route('/del/<int:id_empleado>', methods=['DELETE'])(del_empleado)
+app.route('/empleados_del/<int:id_empleado>', methods=['DELETE'])(del_empleado)
 app.route("/empleado/<int:id_empleado>", methods=["PATCH"])(update_empleado)
 
 if __name__ == '__main__':

347b6f0e236ac65886512d5dd8d11c85f30af653
Anismelow <anyellmendozalopez@gmail.com>
2023-06-22 11:19:55 +0200

    get y get_id hechos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.empleados import *
 from routes.cliente import *
+from routes.pagos import *
 
 app = Flask(__name__)
 
@@ -10,6 +11,14 @@ app.route('/empleados', methods=['GET'])(get_empleados)
 app.route('/empleado/<int:id_empleado>')(obtener_empleado_por_id)
 app.route('/empleados_del/<int:id_empleado>', methods=['DELETE'])(del_empleado)
 app.route("/empleado/<int:id_empleado>", methods=["PATCH"])(update_empleado)
+
+
+#PAGOS
+app.route("/pagos", methods=["GET"])(get_payments)
+
+app.route("/pago/<int:id_pago>", methods=["GET"])(get_payment_by_id)
+
+
 
 # Roots cliente nueva
 app.route('/cliente_add', methods=['POST'])(add_cliente)
